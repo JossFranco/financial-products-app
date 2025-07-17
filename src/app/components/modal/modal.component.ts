4aafb5beeaebf2f5e01e1fe93bdadc6c7d4023ac
JossFranco <jossfranco70@gmail.com>
2025-07-17 15:36:30 -0500

    feate: delete product

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -11,8 +11,16 @@ export class ModalComponent {
   @Input() open: boolean = true;
   @Input() title: string = '';
   @Input() content: string = '';
+  @Input() showFooter: boolean = false;
+  @Output() confirmClick = new EventEmitter<void>();
+  @Output() cancelClick = new EventEmitter<void>();
+
+  confirm() {
+    this.confirmClick.emit();
+  }
 
   close() {
     this.open = false;
+    this.cancelClick.emit();
   }
 }
--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -1,29 +1,63 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalComponent } from '../modal/modal.component';
+import { Product } from '../../interfaces/product.interface';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-options',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent],
   templateUrl: './options.component.html',
   styleUrl: './options.component.scss'
 })
 export class OptionsComponent {
-  @Input() productId: string = '';
+  @Input() product?: Product;
 
   isMenuOpen: boolean = false;
 
-  constructor(private router: Router) { }
+  modalOpen: boolean = false;
+  modalTitle: string = '';
+  modalContent: string = '';
+  modalFooter: boolean = false;
+
+  constructor(
+    private router: Router,
+    private productService: ProductService
+  ) { }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
   editProduct() {
-    this.router.navigate([`edit/${this.productId}`]);
+    this.router.navigate([`edit/${this.product?.id}`]);
   }
 
   deleteProduct() {
     this.isMenuOpen = false;
+    this.modalFooter = true;
+    this.modalTitle = "Confirmar eliminación";
+    this.modalContent = `¿Estás seguro de que deseas eliminar el producto ${this.product?.name}?`;
+    this.modalOpen = true;
+  }
+
+  onDeleteConfirm() {
+    this.modalOpen = false;
+    this.productService.deleteProduct(this.product!.id).subscribe({
+      next: () => {
+        this.router.navigate(['/'], { onSameUrlNavigation: 'reload' });
+      },
+      error: () => {
+        this.modalTitle = "¡Ups algo salio mal!";
+        this.modalContent = "No pudimos eliminar el producto. Por favor, intenta nuevamente más tarde.";
+        this.modalFooter = false;
+        this.modalOpen = true;
+      },
+    });
+  }
+
+  onCancel() {
+    this.modalOpen = false;
   }
 }
